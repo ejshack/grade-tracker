f2c27be760de7979d4f999545a8eba83d954d9cc
bcannon <bcannon@iastate.edu>
2016-02-12 16:10:54 -0600

    Added client to project

---
+++ Client.java
@@ -0,0 +1,21 @@
+package com.g10.portfolio1;
+
+public class Client {
+
+	public static void main(String[] args) {
+		//Try to connect with the server
+		
+		//Show login screen
+		
+		//Validate user info
+		
+		//Kill login thread
+		//Show main screen
+		
+	}
+
+	public static boolean sendToServer() {
+		//TODO add functionality
+		return false;
+	}
+}
